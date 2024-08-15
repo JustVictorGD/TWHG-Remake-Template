06ec18e71a76599b90bab12820e45a48a4b99ab1
VictorGD <147835959+JustVictorGD@users.noreply.github.com>
2024-08-15 22:45:07 +0300

    File structure rework
    - All resources of the game (assets, scripts, object scenes) are placed inside "core"
    
    - The actual levels are placed inside "game"
    
    - There are no longer 10+ scripts stored inside a single folder.
    
    - Removed the "! favorites" folder because I realized there's already a way to favorite files in Godot.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# TWHG-Remake-Template
- 
+This is an open-source Godot project meant to be a great starting point
+for those wanting to make their own remake of The World's Hardest Game.
+
+Discord server, recommended to join: http://twhg.info/discord
+
+Quick project overview:
+	The "core" folder contains various resources that make the game function.
+	The "game" folder contains the gameplay: Areas and levels.
---
+++ core/scripts/object_scripts/circle.gd
@@ -0,0 +1,49 @@
+extends Node
+## Radius from the circle object, where the objects will be created.
+@export var radius : float = 1
+## Amount of objects, which the circle will create.
+@export var object_count : int = 1
+## Angle of the arc, in which the objects will be created. 360 degrees is a full circle.
+@export var arc_degrees : float = 360
+## Type of object, which the circle will create.
+@export var object_type : obj = obj.ENEMIES
+## If this is false, then there will be an object at the start and at the end of the arc. However, if arc_degrees = 360, then the start and end overlap and the object count looks like it's reduced by 1. Changing this to true will make the circle work like in Edit1 and Edit3.
+@export var full_circle_mode : bool = true
+
+var arc : float
+var children : Array[Node]
+
+enum obj {
+	ENEMIES,
+	COINS
+}
+
+func _ready() -> void:
+	
+	GameLoop.animation_update.connect(animation_update)
+	
+	# Makes the orange square invisible in-game
+	$Sprite2D.modulate.a = 0
+	
+	# Loads the chosen object
+	var object: PackedScene
+	if object_type == obj.ENEMIES:
+		object = preload("res://core/objects/blue_enemy.tscn")
+	elif object_type == obj.COINS:
+		object = preload("res://core/objects/coin.tscn")
+	
+	# Creates the objects
+	for i: int in range(object_count):
+		var new_object : Node2D = object.instantiate()
+		add_child(new_object)
+		children.append(new_object)
+
+func animation_update() -> void:
+	arc = deg_to_rad(arc_degrees)
+	
+	for i: int in range(children.size()):
+		if children[i] is Node2D:
+			if full_circle_mode:
+				children[i].position = Vector2(48 * radius, 0).rotated(lerp(0.0, arc, float(i) / object_count))
+			else:
+				children[i].position = Vector2(48 * radius, 0).rotated(lerp(0.0, arc, float(i) / (object_count - 1)))
