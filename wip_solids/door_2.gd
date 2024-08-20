28f5aaafaa78a5327ad89734792259547121bf4a
VictorGD <147835959+JustVictorGD@users.noreply.github.com>
2024-08-20 13:48:38 +0300

    Add "merge_sprite" parameter to Solid2
    - The "merge_sprite" parameter allows you to change the solid's opacity as if it was a single sprite, but prevents it from nicely merging with other solids.

--- wip_solids/door_2.gd
+++ wip_solids/door_2.gd
@@ -1,11 +1,48 @@
+@tool
 extends Solid2
 class_name Door2
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+
+# ## Controls the starting state of the door. If this is false, the door will close when triggered, instead of opening.
+# @export var starts_closed: bool = true
+## Controls the opening direction or style.
+@export var open_method: open_methods = open_methods.RIGHT
+
+## Length of the opening animation in ticks (1/240 seconds).
+@export var open_time: int = 240
+
+## Length of the closing animation in ticks (1/240 seconds).
+@export var close_time: int = 120
+
+## Choose whether or not the gate fades away as it opens.
+@export var fade: bool = false
+
+enum open_methods {
+	UP, LEFT, DOWN, RIGHT, SHRINK, NONE
+}
+
+var open_timer: TickBasedTimer = TickBasedTimer.new(open_time)
+var close_timer: TickBasedTimer = TickBasedTimer.new(close_time)
+
+var is_triggered: bool = false
+
+
+func child_ready() -> void:
+	open_timer.timeout.connect(open_timeout)
+	
+	close_timer.reversed = true
+
+func trigger_door() -> void:
+	pass
+
+func untrigger_door() -> void:
+	pass
+
+
+func open_timeout() -> void:
+	pass
+
+func close_timeout() -> void:
 	pass
--- wip_solids/solid_2.gd
+++ wip_solids/solid_2.gd
@@ -2,30 +2,46 @@
 extends ColorRect
 class_name Solid2
 
-@export var tracking: bool = false
-
+## Allows global_opacity to be used to make the solid semi-transparent without
+## the opacities of the 2 layers stacking, but forces them to be on a single
+## Z-index, preventing them from nicely being merged with other solids.
+@export var merge_sprite: bool = false
+## Only works if merge_sprite is true. Reduces the solid's opacity as if it was a single sprite.
+@export_range(0, 1) var global_opacity: float = 1
 @export var outline_color: Color = Color.BLACK
 @export var fill_color: Color = Color.WHITE
-@export var has_collision: bool = false
+@export var has_collision: bool = true
 
 @export_category("Outline width")
 @export_range(0, 65536) var outwards: int = 3
 @export_range(0, 65536) var inwards: int = 3
 
+var canvas_group: CanvasGroup = CanvasGroup.new()
 var outline: ColorRect = ColorRect.new()
 var fill: ColorRect = ColorRect.new()
+
 var hitbox_index: int
 
 func _ready() -> void:
-	if Engine.is_editor_hint():
-		for child: Node in get_children():
-			child.queue_free()
+	canvas_group.name = "CanvasGroup"
+	outline.name = "Outline"
+	fill.name = "Fill"
 	
-	add_child(outline)
-	add_child(fill)
+	for child: Node in get_children():
+		child.queue_free()
+	
+	if merge_sprite and canvas_group not in get_children():
+		add_child(canvas_group)
+		canvas_group.add_child(outline)
+		canvas_group.add_child(fill)
+	
+	if not merge_sprite and canvas_group in get_children():
+		add_child(outline)
+		add_child(fill)
+		fill.z_index = 1
 	
 	self.z_index = 5
-	outline.z_index = -1
+	#outline.z_index = -1
 	color = Color.TRANSPARENT
 	
 	if not Engine.is_editor_hint():
@@ -34,9 +50,20 @@ func _ready() -> void:
 		if has_collision:
 			hitbox_index = Collider.walls.size()
 			Collider.walls.append(Rect2(NAN, NAN, NAN, NAN))
+	
+	child_ready()
 
 
 func wall_update() -> void:
+	
+	
+	#remove_child(canvas_group)
+	
+	#print(canvas_group.get_children(true))
+	#print(outline, ", ", fill)
+	#print()
+	canvas_group.self_modulate.a = global_opacity
+	
 	outline.color = outline_color
 	outline.position = Vector2(-outwards, -outwards)
 	outline.size = self.size + Vector2(outwards * 2, outwards * 2)
@@ -45,17 +72,37 @@ func wall_update() -> void:
 	fill.position = Vector2(inwards, inwards)
 	fill.size = self.size - Vector2(inwards * 2, inwards * 2)
 	
-	self.pivot_offset = self.size / 2
-	
 	if not Engine.is_editor_hint():
 		Collider.walls[hitbox_index] = Rect2(outline.position + self.position, outline.size)
 
+
+# Override these functions to add more behavior.
+func child_ready() -> void:
+	pass
+
+func update_timers() -> void:
+	pass
 
 # Only happens in the Godot editor, not in game.
 func _process(delta: float) -> void:
 	if Engine.is_editor_hint():
 		wall_update()
+		pass
 		
-		if tracking:
-			print(get_children())
-			print()
+		if merge_sprite and outline in self.get_children() and canvas_group not in get_children():
+			print("HUH")
+			remove_child(outline)
+			remove_child(fill)
+			add_child(canvas_group)
+			canvas_group.add_child(outline)
+			canvas_group.add_child(fill)
+			fill.z_index = 0
+		
+		if not merge_sprite and outline not in self.get_children() and canvas_group in get_children():
+			print("WAH")
+			remove_child(canvas_group)
+			canvas_group.remove_child(outline)
+			canvas_group.remove_child(fill)
+			self.add_child(outline)
+			self.add_child(fill)
+			fill.z_index = 1
