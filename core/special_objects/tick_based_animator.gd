848c943c63268ecbbf809bc47d40441e65ea936a
VictorGD <147835959+JustVictorGD@users.noreply.github.com>
2024-12-05 05:17:41 +0200

    Add BPM property to TickbasedAnimators
    Functions as a "seconds per minute" modifier. You can change it to the BPM of a song and use 1 second as 1 beat for perfect sync.
    
    The new spinner in the first room is perfectly synced to Eric Prydz - Pjanoo.

--- core/special_objects/tick_based_animator.gd
+++ core/special_objects/tick_based_animator.gd
@@ -2,6 +2,9 @@ extends AnimationPlayer
 class_name TickBasedAnimator
 
 @export var start_time: int = 0
+## Determines how many seconds the animation advances per minute.
+## Change this value and use 1 second as 1 beat for music sync.
+@export var bpm: float = 60
 # For some reason advance() doesn't work inside the _ready()
 # function so I'm doing a one-time event myself.
 var started: bool = false
@@ -15,5 +18,5 @@ func animation_update() -> void:
 		advance(GameLoop.TICK_LENGTH * start_time)
 		started = true
 	
-	advance(GameLoop.TICK_LENGTH)
+	advance(GameLoop.TICK_LENGTH * (bpm / 60))
 	pass
