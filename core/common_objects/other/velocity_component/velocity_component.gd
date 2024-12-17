022558465f45c9b3b6f361e421962eabba4bfc88
audigamer <audigamer12@gmail.com>
2024-12-17 16:43:51 +0200

    Add angular velocity to VelocityComponent

--- core/common_objects/other/velocity_component/velocity_component.gd
+++ core/common_objects/other/velocity_component/velocity_component.gd
@@ -2,6 +2,7 @@ class_name VelocityComponent
 extends Node2D
 
 @export var velocity: Vector2 = Vector2.ZERO
+@export var angular_velocity: float = 0
 @export var enabled: bool = true
 @onready var parent: Node = self.get_parent()
 
@@ -12,4 +13,5 @@ func _init(_velocity: Vector2 = velocity) -> void:
 func movement_update() -> void:
 	if parent is Node2D and enabled:
 		parent.position += velocity
+		parent.rotation_degrees += angular_velocity
 	
