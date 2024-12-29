d0e0784d698ac10a80a9953e595bc85f7d1c4178
VictorGD <147835959+JustVictorGD@users.noreply.github.com>
2024-12-29 13:07:18 +0200

    Add GameObject2D

---
+++ core/game_objects/game_object_2d.gd
@@ -0,0 +1,60 @@
+@tool
+extends Node2D
+class_name GameObject2D
+
+## Required for automatic theme support and color updates.
+@export var sprite: MaskedSprite
+
+## Makes colors constantly get checked in game. Probably costs some
+## performance but allows themes with gradually changing colors.
+@export var rapid_update_colors: bool = false
+
+## If the owner of this node is of the 'Area' class, try to link
+## the object's colors to the area's 'AreaTheme' resource.
+@export var copy_area_theme: bool = true
+
+## The exact name of a variable in an 'AreaTheme'
+## resource to reference to color the sprite's outline.
+@export var outline_theme_link: String = ""
+
+## The exact name of a variable in an 'AreaTheme'
+## resource to reference to color the sprite's filling.
+@export var fill_theme_link: String = ""
+
+@export_category("Self-contained theme")
+
+@export var outline_color: Color = Color.BLACK
+@export var fill_color: Color = Color.WHITE
+
+# It's not only clean but I think it's also more performant to
+# call the function once and store its output in a variable.
+@onready var in_editor: bool = Engine.is_editor_hint()
+
+
+func _ready() -> void:
+	if sprite == null:
+		return
+	
+	sprite.rapid_update_colors = rapid_update_colors
+
+
+func _process(delta: float) -> void:
+	if sprite == null or not (rapid_update_colors or in_editor):
+		return
+	
+	var theme: AreaTheme
+	
+	if owner is Area:
+		theme = owner.theme
+	
+	if theme == null or not copy_area_theme:
+		sprite.outline_color = outline_color
+		sprite.fill_color = fill_color
+	else:
+		if outline_theme_link != "":
+			sprite.outline_color = theme.get(outline_theme_link)
+		
+		if fill_theme_link != "":
+			sprite.fill_color = theme.get(fill_theme_link)
+	
+	sprite.update_colors()
