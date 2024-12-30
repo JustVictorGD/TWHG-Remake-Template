777221e0013b609675818bf16de01cac2b387695
audigamer <audigamer12@gmail.com>
2024-12-30 22:15:35 +0200

    Update paint select appearance
    Add outline to each paint button and adjust its size

--- core/system/interface/pause_menu/paint_container/paint_button.gd
+++ core/system/interface/pause_menu/paint_container/paint_button.gd
@@ -5,13 +5,21 @@ var paint_id: int
 
 func _ready() -> void:
 	button_down.connect(on_click)
-	await get_parent().ready
-	custom_minimum_size = Vector2(50, 50)
+	#await get_parent().ready
+	#custom_minimum_size = Vector2(24, 24)
 	
 	if paint_id == -1:
-		modulate = PaintManager.default_player.fill 
+		self_modulate = PaintManager.default_player.fill
+		change_outline_modulate(PaintManager.default_player.outline)
 	else:
-		modulate = PaintManager.unlockable_paints[paint_id].fill
+		self_modulate = PaintManager.unlockable_paints[paint_id].fill
+		change_outline_modulate(PaintManager.unlockable_paints[paint_id].outline)
+		
 
 func on_click() -> void:
 	print(paint_id) # Does this for now, will make it actually change the player color tomorrow
+
+func change_outline_modulate(color: Color) -> void:
+	print(color)
+	for child: Node in get_children():
+		child.color = color
