29da175e2d25e926332c3b5f25e7939784a86e9f
VictorGD <147835959+JustVictorGD@users.noreply.github.com>
2024-12-31 12:34:49 +0200

    Make end screen possible to exit

--- core/system/scene/end_screen.gd
+++ core/system/scene/end_screen.gd
@@ -4,10 +4,17 @@ extends Node2D
 @onready var flash_timer: TickBasedTimer = $TickBasedTimer
 
 
+func _ready() -> void:
+	GameManager.current_level = "end_screen"
+	$Button.pressed.connect(on_button_pressed)
+
+
 func _process(delta: float) -> void:
+	print($Button.is_connected("pressed", on_button_pressed))
+	
 	flash.color.a = flash_timer.get_progress_left()
 
 
-func _on_button_pressed() -> void:
+func on_button_pressed() -> void:
 	print("pressing the button")
 	get_tree().change_scene_to_packed(preload("res://game/scenes/menu.tscn"))
